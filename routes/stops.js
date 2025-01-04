f677e30048e673c78a8ebf0e99fde6387ba61668
Baramex <contact@baramex.me>
2025-01-04 18:43:08 +0100

    [TASK] Sort stops by distance

--- routes/stops.js
+++ routes/stops.js
@@ -1,3 +1,4 @@
+const Location = require("../modules/Location");
 const LocationInformation = require("../modules/LocationInformation");
 
 async function getNearStops(req, res) {
@@ -6,6 +7,7 @@ async function getNearStops(req, res) {
         const lat = req.query.lat;
         if (!lon || !lat) return res.status(400).json({ error: "Missing required parameters lon and lat" });
         const stops = await LocationInformation.getNearStops(lon, lat);
+        stops.sort((a, b) => Location.getDistance(lon, lat, a.lon, a.lat) - Location.getDistance(lon, lat, b.lon, b.lat));
         res.json(stops);
     } catch (error) {
         console.error(error);
